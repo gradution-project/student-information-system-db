b4a66afa24e2fb32272c7f8cc77232fa5f74924e
Agit Rubar Demir <demiragitrubar@gmail.com>
2021-11-02 22:24:50 +0300

    Auth DB Has Been Merged to SIS DB & Table Names Have Been Fixed and Updated

--- mysql/sql/sis/V1_0__AUTH_STUDENT_CREATE_TABLE.sql
+++ mysql/sql/sis/V1_0__AUTH_STUDENT_CREATE_TABLE.sql
@@ -1,11 +1,11 @@
 # --------------------------------------------
-# DDL for Table OFFICER_SIGN_IN
+# DDL for Table STUDENT_SIGN_IN
 # --------------------------------------------
-CREATE TABLE OFFICER_SIGN_IN
+CREATE TABLE AUTH_STUDENT_SIGN_IN
 (
     ID              BIGINT PRIMARY KEY UNIQUE AUTO_INCREMENT,
-    OFFICER_ID      DECIMAL(7) UNIQUE,
+    STUDENT_ID      DECIMAL(12) UNIQUE,
     PASSWORD        VARCHAR(512),
     FAIL_COUNTER    DECIMAL(1),
     LAST_LOGIN_DATE TIMESTAMP
-) TABLESPACE AUTH_TBS;
+) TABLESPACE SIS_TBS;
--- mysql/sql/sis/V1_2__AUTH_OFFICER_CREATE_TABLE.sql
+++ mysql/sql/sis/V1_2__AUTH_OFFICER_CREATE_TABLE.sql
@@ -1,11 +1,11 @@
 # --------------------------------------------
-# DDL for Table STUDENT_SIGN_IN
+# DDL for Table OFFICER_SIGN_IN
 # --------------------------------------------
-CREATE TABLE STUDENT_SIGN_IN
+CREATE TABLE AUTH_OFFICER_SIGN_IN
 (
     ID              BIGINT PRIMARY KEY UNIQUE AUTO_INCREMENT,
-    STUDENT_ID      DECIMAL(12) UNIQUE,
+    OFFICER_ID      DECIMAL(7) UNIQUE,
     PASSWORD        VARCHAR(512),
     FAIL_COUNTER    DECIMAL(1),
     LAST_LOGIN_DATE TIMESTAMP
-) TABLESPACE AUTH_TBS;
+) TABLESPACE SIS_TBS;
