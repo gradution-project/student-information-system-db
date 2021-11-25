1408c5e1cbe0909afb3a2025291bf30a250f4cc8
Agit Rubar Demir <demiragitrubar@gmail.com>
2021-11-25 22:22:19 +0300

    AUTH Table Names Have Been Fixed

--- mysql/sql/sis/V1_0__AUTH_CREATE_TABLE.sql
+++ mysql/sql/sis/V1_0__AUTH_CREATE_TABLE.sql
@@ -1,7 +1,7 @@
 # --------------------------------------------
-# DDL for Table AUTH_STUDENT_SIGN_IN
+# DDL for Table AUTH_STUDENT_LOGIN
 # --------------------------------------------
-CREATE TABLE AUTH_STUDENT_SIGN_IN
+CREATE TABLE AUTH_STUDENT_LOGIN
 (
     ID              BIGINT PRIMARY KEY UNIQUE AUTO_INCREMENT,
     STUDENT_ID      DECIMAL(12) UNIQUE,
@@ -11,9 +11,9 @@ CREATE TABLE AUTH_STUDENT_SIGN_IN
 ) TABLESPACE SIS_TBS;
 
 # --------------------------------------------
-# DDL for Table AUTH_TEACHER_SIGN_IN
+# DDL for Table AUTH_TEACHER_LOGIN
 # --------------------------------------------
-CREATE TABLE AUTH_TEACHER_SIGN_IN
+CREATE TABLE AUTH_TEACHER_LOGIN
 (
     ID              BIGINT PRIMARY KEY UNIQUE AUTO_INCREMENT,
     TEACHER_ID      DECIMAL(10) UNIQUE,
@@ -23,9 +23,9 @@ CREATE TABLE AUTH_TEACHER_SIGN_IN
 ) TABLESPACE SIS_TBS;
 
 # --------------------------------------------
-# DDL for Table AUTH_OFFICER_SIGN_IN
+# DDL for Table AUTH_OFFICER_LOGIN
 # --------------------------------------------
-CREATE TABLE AUTH_OFFICER_SIGN_IN
+CREATE TABLE AUTH_OFFICER_LOGIN
 (
     ID              BIGINT PRIMARY KEY UNIQUE AUTO_INCREMENT,
     OFFICER_ID      DECIMAL(7) UNIQUE,
