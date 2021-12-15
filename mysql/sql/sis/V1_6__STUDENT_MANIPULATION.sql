6830189a3de38c39ca9c69bd9fe660fa7662a709
Agit Rubar Demir <demiragitrubar@gmail.com>
2021-12-15 20:37:57 +0300

    Student Manipulations Have Been Fixed

--- mysql/sql/sis/V1_6__STUDENT_MANIPULATION.sql
+++ mysql/sql/sis/V1_6__STUDENT_MANIPULATION.sql
@@ -2,127 +2,108 @@
 # Insert SQL for Table STUDENT_ACADEMIC_INFO
 # --------------------------------------------
 INSERT INTO sis.STUDENT_ACADEMIC_INFO (STUDENT_ID, DEPARTMENT_ID, DEGREE, CLASS_LEVEL, EMAIL, STATUS, REGISTRATION_DATE,
-                                       CREATED_DATE, CREATED_USER_ID, MODIFIED_DATE, MODIFIED_USER_ID)
+                                       CREATED_DATE, CREATED_USER_ID)
 VALUES ('200011101210', '11012', 'DOCTORAL', 'FIRST', 'eren.korkmaz@ogr.university.edu.tr', 'ACTIVE',
-        '2021-11-23 15:26:11', '2021-11-23 15:26:11', '1', '2021-11-23 15:26:11', '1');
+        '2021-11-23 15:26:11', '2021-11-23 15:26:11', '1');
 
 INSERT INTO sis.STUDENT_ACADEMIC_INFO (STUDENT_ID, DEPARTMENT_ID, DEGREE, CLASS_LEVEL, EMAIL, STATUS, REGISTRATION_DATE,
-                                       CREATED_DATE, CREATED_USER_ID, MODIFIED_DATE, MODIFIED_USER_ID)
+                                       CREATED_DATE, CREATED_USER_ID)
 VALUES ('200011101211', '11012', 'DOCTORAL', 'FIRST', 'baris.altun@ogr.university.edu.tr', 'ACTIVE',
-        '2021-11-23 15:26:11', '2021-11-23 15:26:11', '1', '2021-11-23 15:26:11', '1');
+        '2021-11-23 15:26:11', '2021-11-23 15:26:11', '1');
 
 INSERT INTO sis.STUDENT_ACADEMIC_INFO (STUDENT_ID, DEPARTMENT_ID, DEGREE, CLASS_LEVEL, EMAIL, STATUS, REGISTRATION_DATE,
-                                       CREATED_DATE, CREATED_USER_ID, MODIFIED_DATE, MODIFIED_USER_ID)
+                                       CREATED_DATE, CREATED_USER_ID)
 VALUES ('200011101212', '11012', 'DOCTORAL', 'FIRST', 'mehmet.kapukaya@ogr.university.edu.tr', 'ACTIVE',
-        '2021-11-23 15:26:11', '2021-11-23 15:26:11', '1', '2021-11-23 15:26:11', '1');
+        '2021-11-23 15:26:11', '2021-11-23 15:26:11', '1');
 
 INSERT INTO sis.STUDENT_ACADEMIC_INFO (STUDENT_ID, DEPARTMENT_ID, DEGREE, CLASS_LEVEL, EMAIL, STATUS, REGISTRATION_DATE,
-                                       CREATED_DATE, CREATED_USER_ID, MODIFIED_DATE, MODIFIED_USER_ID)
+                                       CREATED_DATE, CREATED_USER_ID)
 VALUES ('200011101213', '11012', 'DOCTORAL', 'FIRST', 'umut.bilgin@ogr.university.edu.tr', 'ACTIVE',
-        '2021-11-23 15:26:11', '2021-11-23 15:26:11', '1', '2021-11-23 15:26:11', '1');
+        '2021-11-23 15:26:11', '2021-11-23 15:26:11', '1');
 
 INSERT INTO sis.STUDENT_ACADEMIC_INFO (STUDENT_ID, DEPARTMENT_ID, DEGREE, CLASS_LEVEL, EMAIL, STATUS, REGISTRATION_DATE,
-                                       CREATED_DATE, CREATED_USER_ID, MODIFIED_DATE, MODIFIED_USER_ID)
+                                       CREATED_DATE, CREATED_USER_ID)
 VALUES ('200011101214', '11012', 'DOCTORAL', 'FIRST', 'berfin.bural@ogr.university.edu.tr', 'ACTIVE',
-        '2021-11-23 15:26:11', '2021-11-23 15:26:11', '1', '2021-11-23 15:26:11', '1');
+        '2021-11-23 15:26:11', '2021-11-23 15:26:11', '1');
 
 INSERT INTO sis.STUDENT_ACADEMIC_INFO (STUDENT_ID, DEPARTMENT_ID, DEGREE, CLASS_LEVEL, EMAIL, STATUS, REGISTRATION_DATE,
-                                       CREATED_DATE, CREATED_USER_ID, MODIFIED_DATE, MODIFIED_USER_ID)
+                                       CREATED_DATE, CREATED_USER_ID)
 VALUES ('200011101215', '11012', 'DOCTORAL', 'FIRST', 'ahmet.kaya@ogr.university.edu.tr', 'ACTIVE',
-        '2021-11-23 15:26:11', '2021-11-23 15:26:11', '1', '2021-11-23 15:26:11', '1');
+        '2021-11-23 15:26:11', '2021-11-23 15:26:11', '1');
 
 INSERT INTO sis.STUDENT_ACADEMIC_INFO (STUDENT_ID, DEPARTMENT_ID, DEGREE, CLASS_LEVEL, EMAIL, STATUS, REGISTRATION_DATE,
-                                       CREATED_DATE, CREATED_USER_ID, MODIFIED_DATE, MODIFIED_USER_ID)
+                                       CREATED_DATE, CREATED_USER_ID)
 VALUES ('200011101216', '11012', 'DOCTORAL', 'FIRST', 'beril.korkut@ogr.university.edu.tr', 'ACTIVE',
-        '2021-11-23 15:26:11', '2021-11-23 15:26:11', '1', '2021-11-23 15:26:11', '1');
+        '2021-11-23 15:26:11', '2021-11-23 15:26:11', '1');
 
 INSERT INTO sis.STUDENT_ACADEMIC_INFO (STUDENT_ID, DEPARTMENT_ID, DEGREE, CLASS_LEVEL, EMAIL, STATUS, REGISTRATION_DATE,
-                                       CREATED_DATE, CREATED_USER_ID, MODIFIED_DATE, MODIFIED_USER_ID)
+                                       CREATED_DATE, CREATED_USER_ID)
 VALUES ('200011101217', '11012', 'DOCTORAL', 'FIRST', 'sümeyye.can@ogr.university.edu.tr', 'ACTIVE',
-        '2021-11-23 15:26:11', '2021-11-23 15:26:11', '1', '2021-11-23 15:26:11', '1');
+        '2021-11-23 15:26:11', '2021-11-23 15:26:11', '1');
 
 INSERT INTO sis.STUDENT_ACADEMIC_INFO (STUDENT_ID, DEPARTMENT_ID, DEGREE, CLASS_LEVEL, EMAIL, STATUS, REGISTRATION_DATE,
-                                       CREATED_DATE, CREATED_USER_ID, MODIFIED_DATE, MODIFIED_USER_ID)
+                                       CREATED_DATE, CREATED_USER_ID)
 VALUES ('200011101218', '11012', 'DOCTORAL', 'FIRST', 'betül.bilir@ogr.university.edu.tr', 'ACTIVE',
-        '2021-11-23 15:26:11', '2021-11-23 15:26:11', '1', '2021-11-23 15:26:11', '1');
+        '2021-11-23 15:26:11', '2021-11-23 15:26:11', '1');
 
 INSERT INTO sis.STUDENT_ACADEMIC_INFO (STUDENT_ID, DEPARTMENT_ID, DEGREE, CLASS_LEVEL, EMAIL, STATUS, REGISTRATION_DATE,
-                                       CREATED_DATE, CREATED_USER_ID, MODIFIED_DATE, MODIFIED_USER_ID)
+                                       CREATED_DATE, CREATED_USER_ID)
 VALUES ('200011101219', '11012', 'DOCTORAL', 'FIRST', 'yusuf.atmaca@ogr.university.edu.tr', 'ACTIVE',
-        '2021-11-23 15:26:11', '2021-11-23 15:26:11', '1', '2021-11-23 15:26:11', '1');
+        '2021-11-23 15:26:11', '2021-11-23 15:26:11', '1');
 
 
 # --------------------------------------------
 # Insert SQL for Table STUDENT_PERSONAL_INFO
 # --------------------------------------------
 INSERT INTO sis.STUDENT_PERSONAL_INFO (STUDENT_ID, TC_NO, NAME, SURNAME, EMAIL, PHONE_NUMBER, STATUS, PROFILE_PHOTO,
-                                       PROFILE_PHOTO_URL, BIRTHDAY, ADDRESS, CREATED_DATE, CREATED_USER_ID,
-                                       MODIFIED_DATE, MODIFIED_USER_ID)
-VALUES ('200011101210', '15963256874', 'Eren', 'Korkmaz', 'eren.korkmaz@sisgmail.com', '05366847893', 'ACTIVE', null,
-        null,
-        '2021-11-23 19:52:22', 'Ankara', '2021-11-23 19:52:22', '1', '2021-11-23 19:52:22', '1');
+                                       PROFILE_PHOTO_URL, BIRTHDAY, ADDRESS, CREATED_DATE, CREATED_USER_ID)
+VALUES ('200011101210', '15963256874', 'Eren', 'Korkmaz', 'eren.korkmaz@sisgmail.com', '05366847893', 'ACTIVE',
+        null, null, '2021-11-23 19:52:22', 'Ankara', '2021-11-23 19:52:22', '1');
 
 INSERT INTO sis.STUDENT_PERSONAL_INFO (STUDENT_ID, TC_NO, NAME, SURNAME, EMAIL, PHONE_NUMBER, STATUS, PROFILE_PHOTO,
-                                       PROFILE_PHOTO_URL, BIRTHDAY, ADDRESS, CREATED_DATE, CREATED_USER_ID,
-                                       MODIFIED_DATE, MODIFIED_USER_ID)
-VALUES ('200011101211', '15975368521', 'Barış', 'Altun', 'baris.altun@sisgmail.com', '05366847825', 'ACTIVE', null,
-        null,
-        '2021-11-23 19:52:22', 'Ankara', '2021-11-23 19:52:22', '1', '2021-11-23 19:52:22', '1');
+                                       PROFILE_PHOTO_URL, BIRTHDAY, ADDRESS, CREATED_DATE, CREATED_USER_ID)
+VALUES ('200011101211', '15975368521', 'Barış', 'Altun', 'baris.altun@sisgmail.com', '05366847825', 'ACTIVE',
+        null, null, '2021-11-23 19:52:22', 'Ankara', '2021-11-23 19:52:22', '1');
 
 INSERT INTO sis.STUDENT_PERSONAL_INFO (STUDENT_ID, TC_NO, NAME, SURNAME, EMAIL, PHONE_NUMBER, STATUS, PROFILE_PHOTO,
-                                       PROFILE_PHOTO_URL, BIRTHDAY, ADDRESS, CREATED_DATE, CREATED_USER_ID,
-                                       MODIFIED_DATE, MODIFIED_USER_ID)
+                                       PROFILE_PHOTO_URL, BIRTHDAY, ADDRESS, CREATED_DATE, CREATED_USER_ID)
 VALUES ('200011101212', '32165474125', 'Mehmet', 'Kapukaya', 'mehmet.kapukaya@sisgmail.com', '05366847819', 'ACTIVE',
-        null,
-        null, '2021-11-23 19:52:22', 'Ankara', '2021-11-23 19:52:22', '1', '2021-11-23 19:52:22', '1');
+        null, null, '2021-11-23 19:52:22', 'Ankara', '2021-11-23 19:52:22', '1');
 
 INSERT INTO sis.STUDENT_PERSONAL_INFO (STUDENT_ID, TC_NO, NAME, SURNAME, EMAIL, PHONE_NUMBER, STATUS, PROFILE_PHOTO,
-                                       PROFILE_PHOTO_URL, BIRTHDAY, ADDRESS, CREATED_DATE, CREATED_USER_ID,
-                                       MODIFIED_DATE, MODIFIED_USER_ID)
-VALUES ('200011101213', '45696321457', 'Umut', 'Bilgin', 'umut.bilgin@sisgmail.com', '05366847854', 'ACTIVE', null,
-        null,
-        '2021-11-23 19:52:22', 'Ankara', '2021-11-23 19:52:22', '1', '2021-11-23 19:52:22', '1');
+                                       PROFILE_PHOTO_URL, BIRTHDAY, ADDRESS, CREATED_DATE, CREATED_USER_ID)
+VALUES ('200011101213', '45696321457', 'Umut', 'Bilgin', 'umut.bilgin@sisgmail.com', '05366847854', 'ACTIVE',
+        null, null, '2021-11-23 19:52:22', 'Ankara', '2021-11-23 19:52:22', '1');
 
 INSERT INTO sis.STUDENT_PERSONAL_INFO (STUDENT_ID, TC_NO, NAME, SURNAME, EMAIL, PHONE_NUMBER, STATUS, PROFILE_PHOTO,
-                                       PROFILE_PHOTO_URL, BIRTHDAY, ADDRESS, CREATED_DATE, CREATED_USER_ID,
-                                       MODIFIED_DATE, MODIFIED_USER_ID)
-VALUES ('200011101214', '25468795142', 'Berfin', 'Bural', 'berfin.bural@sisgmail.com', '05366847878', 'ACTIVE', null,
-        null,
-        '2021-11-23 19:52:22', 'Ankara', '2021-11-23 19:52:22', '1', '2021-11-23 19:52:22', '1');
+                                       PROFILE_PHOTO_URL, BIRTHDAY, ADDRESS, CREATED_DATE, CREATED_USER_ID)
+VALUES ('200011101214', '25468795142', 'Berfin', 'Bural', 'berfin.bural@sisgmail.com', '05366847878', 'ACTIVE',
+        null, null, '2021-11-23 19:52:22', 'Ankara', '2021-11-23 19:52:22', '1');
 
 INSERT INTO sis.STUDENT_PERSONAL_INFO (STUDENT_ID, TC_NO, NAME, SURNAME, EMAIL, PHONE_NUMBER, STATUS, PROFILE_PHOTO,
-                                       PROFILE_PHOTO_URL, BIRTHDAY, ADDRESS, CREATED_DATE, CREATED_USER_ID,
-                                       MODIFIED_DATE, MODIFIED_USER_ID)
-VALUES ('200011101215', '26585214525', 'Ahmet', 'Kaya', 'ahmet.kaya@sisgmail.com', '05366847847', 'ACTIVE', null, null,
-        '2021-11-23 19:52:22', 'Ankara', '2021-11-23 19:52:22', '1', '2021-11-23 19:52:22', '1');
+                                       PROFILE_PHOTO_URL, BIRTHDAY, ADDRESS, CREATED_DATE, CREATED_USER_ID)
+VALUES ('200011101215', '26585214525', 'Ahmet', 'Kaya', 'ahmet.kaya@sisgmail.com', '05366847847', 'ACTIVE',
+        null, null, '2021-11-23 19:52:22', 'Ankara', '2021-11-23 19:52:22', '1');
 
 INSERT INTO sis.STUDENT_PERSONAL_INFO (STUDENT_ID, TC_NO, NAME, SURNAME, EMAIL, PHONE_NUMBER, STATUS, PROFILE_PHOTO,
-                                       PROFILE_PHOTO_URL, BIRTHDAY, ADDRESS, CREATED_DATE, CREATED_USER_ID,
-                                       MODIFIED_DATE, MODIFIED_USER_ID)
-VALUES ('200011101216', '26359887415', 'Beril', 'Korkut', 'beril.korkut@sisgmail.com', '05366847816', 'ACTIVE', null,
-        null,
-        '2021-11-23 19:52:22', 'Ankara', '2021-11-23 19:52:22', '1', '2021-11-23 19:52:22', '1');
+                                       PROFILE_PHOTO_URL, BIRTHDAY, ADDRESS, CREATED_DATE, CREATED_USER_ID)
+VALUES ('200011101216', '26359887415', 'Beril', 'Korkut', 'beril.korkut@sisgmail.com', '05366847816', 'ACTIVE',
+        null, null, '2021-11-23 19:52:22', 'Ankara', '2021-11-23 19:52:22', '1');
 
 INSERT INTO sis.STUDENT_PERSONAL_INFO (STUDENT_ID, TC_NO, NAME, SURNAME, EMAIL, PHONE_NUMBER, STATUS, PROFILE_PHOTO,
-                                       PROFILE_PHOTO_URL, BIRTHDAY, ADDRESS, CREATED_DATE, CREATED_USER_ID,
-                                       MODIFIED_DATE, MODIFIED_USER_ID)
-VALUES ('200011101217', '13987415623', 'Sümeyye', 'Can', 'sumeyye.can@sisgmail.com', '05366847817', 'ACTIVE', null,
-        null,
-        '2021-11-23 19:52:22', 'Ankara', '2021-11-23 19:52:22', '1', '2021-11-23 19:52:22', '1');
+                                       PROFILE_PHOTO_URL, BIRTHDAY, ADDRESS, CREATED_DATE, CREATED_USER_ID)
+VALUES ('200011101217', '13987415623', 'Sümeyye', 'Can', 'sumeyye.can@sisgmail.com', '05366847817', 'ACTIVE',
+        null, null, '2021-11-23 19:52:22', 'Ankara', '2021-11-23 19:52:22', '1');
 
 INSERT INTO sis.STUDENT_PERSONAL_INFO (STUDENT_ID, TC_NO, NAME, SURNAME, EMAIL, PHONE_NUMBER, STATUS, PROFILE_PHOTO,
-                                       PROFILE_PHOTO_URL, BIRTHDAY, ADDRESS, CREATED_DATE, CREATED_USER_ID,
-                                       MODIFIED_DATE, MODIFIED_USER_ID)
-VALUES ('200011101218', '16325945982', 'Betül', 'Bilir', 'betül.bilir@sisgmail.com', '05366847898', 'ACTIVE', null,
-        null,
-        '2021-11-23 19:52:22', 'Ankara', '2021-11-23 19:52:22', '1', '2021-11-23 19:52:22', '1');
+                                       PROFILE_PHOTO_URL, BIRTHDAY, ADDRESS, CREATED_DATE, CREATED_USER_ID)
+VALUES ('200011101218', '16325945982', 'Betül', 'Bilir', 'betül.bilir@sisgmail.com', '05366847898', 'ACTIVE',
+        null, null, '2021-11-23 19:52:22', 'Ankara', '2021-11-23 19:52:22', '1');
 
 INSERT INTO sis.STUDENT_PERSONAL_INFO (STUDENT_ID, TC_NO, NAME, SURNAME, EMAIL, PHONE_NUMBER, STATUS, PROFILE_PHOTO,
-                                       PROFILE_PHOTO_URL, BIRTHDAY, ADDRESS, CREATED_DATE, CREATED_USER_ID,
-                                       MODIFIED_DATE, MODIFIED_USER_ID)
-VALUES ('200011101219', '34525698512', 'Yusuf', 'Atmaca', 'yusuf.atmaca@sisgmail.com', '05366847811', 'ACTIVE', null,
-        null,
-        '2021-11-23 19:52:22', 'Ankara', '2021-11-23 19:52:22', '1', '2021-11-23 19:52:22', '1');
+                                       PROFILE_PHOTO_URL, BIRTHDAY, ADDRESS, CREATED_DATE, CREATED_USER_ID)
+VALUES ('200011101219', '34525698512', 'Yusuf', 'Atmaca', 'yusuf.atmaca@sisgmail.com', '05366847811', 'ACTIVE',
+        null, null, '2021-11-23 19:52:22', 'Ankara', '2021-11-23 19:52:22', '1');
 
 # --------------------------------------------
 # Insert SQL for Table STUDENT_PARAMETER
